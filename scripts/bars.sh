db8e6a3953570b45f0d30ca45dd502154a762d0c
Joey <joeyshi12@gmail.com>
2023-02-20 02:18:45 -0800

    added volume module

--- scripts/bars.sh
+++ scripts/bars.sh
@@ -2,6 +2,10 @@
 interval=0
 sep=" | "
 
+disk() {
+    printf "󰋊  $(df -h / | awk ' /[0-9]/ {print $3 "/" $2}')"
+}
+
 cpu() {
     cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
     printf "CPU $cpu_val"
@@ -21,6 +25,18 @@ mem() {
     printf " $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
 }
 
+volume() {
+    volume=$(pulsemixer --get-volume | cut -d' ' -f1)
+    if [ "$volume" -lt 33 ]; then
+        printf "󰕿  "
+    elif [ "$volume" -lt 66 ]; then
+        printf "󰖀  "
+    else
+        printf "󰕾  "
+    fi
+    printf "$volume"
+}
+
 wlan() {
     case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
         up) printf "  %s" "Connected" ;;
@@ -36,5 +52,5 @@ while true; do
     [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
     interval=$((interval + 1))
     sleep 1
-    xsetroot -name "  ${updates}${sep}$(cpu)${sep}$(mem)${sep}$(wlan)${sep}$(clock)  "
+    xsetroot -name "  ${updates}${sep}$(cpu)${sep}$(mem)${sep}$(disk)${sep}$(volume)${sep}$(wlan)${sep}$(clock)  "
 done
