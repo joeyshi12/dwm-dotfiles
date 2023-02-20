e89490d88f6b3bfba8af5e26ad1c469b160dbbb1
Joey <joeyshi12@gmail.com>
2023-02-19 23:25:38 -0800

    reformat bars script

--- scripts/bars.sh
+++ scripts/bars.sh
@@ -3,33 +3,31 @@ interval=0
 sep="    |    "
 
 cpu() {
-  cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
-  printf "CPU  "
-  printf "$cpu_val"
+    cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+    printf "CPU  "
+    printf "$cpu_val"
 }
 
 pkg_updates() {
-  #updates=$(doas xbps-install -un | wc -l) # void
-  updates=$(checkupdates 2>/dev/null | wc -l) # arch
-  # updates=$(aptitude search '~U' | wc -l)  # apt (ubuntu,debian etc)
+    updates=$(checkupdates 2>/dev/null | wc -l) # arch
 
-  if [ -z "$updates" ]; then
-    printf "  Fully Updated"
-  else
-    printf "  $updates"" updates"
-  fi
+    if [ -z "$updates" ]; then
+        printf "  Fully Updated"
+    else
+        printf "  $updates"" updates"
+    fi
 }
 
 mem() {
-  printf "  "
-  printf "$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
+    printf "  "
+    printf "$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
 }
 
 wlan() {
-	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "  %s" "Connected" ;;
-	down) printf "  %s" "Disconnected" ;;
-	esac
+    case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
+        up) printf "  %s" "Connected" ;;
+        down) printf "  %s" "Disconnected" ;;
+    esac
 }
 
 clock() {
@@ -37,8 +35,8 @@ clock() {
 }
 
 while true; do
-  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
-  interval=$((interval + 1))
-  sleep 1
-  xsetroot -name "  ${updates}${sep}$(cpu)${sep}$(mem)${sep}$(wlan)${sep}$(clock)  "
+    [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+    interval=$((interval + 1))
+    sleep 1
+    xsetroot -name "  ${updates}${sep}$(cpu)${sep}$(mem)${sep}$(wlan)${sep}$(clock)  "
 done
