12b7daf8f343fc5415881f858e7c7981646e9b84
Joey <joeyshi12@gmail.com>
2023-08-06 12:35:09 -0700

    updated configs

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,58 +1,45 @@
-alias \
-    ls="ls --color=auto" \
-    grep="grep --color=auto" \
-    v="$EDITOR" \
-    sv="sudo -E $EDITOR" \
-    m="neomutt" \
-    copy="xclip -sel c" \
-    diff="diff --color" \
-    ssh="kitty +kitten ssh"  # kitty ssh fix
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias v="$EDITOR"
+alias sv="sudo -E $EDITOR"
+alias m="neomutt"
+alias copy="xclip -sel c"
+alias diff="diff --color"
+alias ssh="kitty +kitten ssh"  # kitty ssh fix
 
-# pacman abbreviations
-alias \
-    pacs="sudo pacman -S" \
-    pacss="pacman -Ss" \
-    pacqs="pacman -Qs" \
-    pacsyu="sudo pacman -Syu" \
-    pacrs="sudo pacman -Rs" \
-    pacsc="sudo pacman -Sc"
+# Pacman
+alias pacs="sudo pacman -S"
+alias pacss="pacman -Ss"
+alias pacqs="pacman -Qs"
+alias pacsyu="sudo pacman -Syu"
+alias pacrsn="sudo pacman -Rsn"
+alias pacsc="sudo pacman -Sc"
 
-pacar() {
-    orphan_packages="$(pacman -Qdtq)"
-    if [ -z "$orphan_packages" ]; then
-        printf "error: no orphan packages found\n\n"
-    else
-        sudo pacman -Rs $orphan_packages
-    fi
-}
+# Git
+alias g="git"
+alias ga="git add"
+alias gaa="git add --all"
+alias grm="git rm --cached"
+alias gb="git branch"
+alias gc="git commit -vm"
+alias gca="git commit -vam"
+alias gcl="git clone --recurse-submodules"
+alias gl="git pull"
+alias gup="git pull --rebase"
+alias gp="git push"
+alias gf="git fetch"
+alias gco="git checkout"
+alias gst="git status"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias glg="git log --graph"
+alias gr="git remote"
 
-# git abbreviations
-alias \
-    g="git" \
-    ga="git add" \
-    gaa="git add --all" \
-    grm="git rm --cached" \
-    gb="git branch" \
-    gc="git commit -vm" \
-    gca="git commit -vam" \
-    gcl="git clone --recurse-submodules" \
-    gl="git pull" \
-    gup="git pull --rebase" \
-    gp="git push" \
-    gf="git fetch" \
-    gco="git checkout" \
-    gst="git status" \
-    gd="git diff" \
-    gdc="git diff --cached" \
-    glg="git log --graph" \
-    gr="git remote"
-
-# tmux abbreviations
-alias \
-    t="tmux" \
-    ta="tmux attach-session" \
-    tl="tmux list-sessions" \
-    tk="tmux kill-session"
+# Tmux
+alias t="tmux"
+alias ta="tmux attach-session"
+alias tl="tmux list-sessions"
+alias tk="tmux kill-session"
 
 # Compiles and executes C file and removes binary
 # $1 path to the C file
